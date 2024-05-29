e1ff41b3be2f26b2b901087a82d806bad4b04b9b
SloMo128 <nesci.alessandro.17@outlook.it>
2024-05-29 12:25:47 +0200

    123

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { FormGroup } from '@angular/forms';
 import { GitHubRepos } from './model/repository';
+import { ApiGithHubServise } from './service/api.github.servise';
 
 @Component({
   selector: 'app-root',
@@ -14,34 +15,88 @@ export class AppComponent implements OnInit {
   userName:string = 'SloMo128';
   baseUrl:string = 'https://api.github.com/';
 
-  repos: GitHubRepos[];
-  errorMessage:string = '';
-  constructor(private http: HttpClient) {}
+  repos: GitHubRepos[]=[];
+  errorMessage: any;
+  errorMessage_msg:string = "L'utente Non esiste";
+  loaded: boolean;
+  no_data_found: boolean;
+  no_data_found_msg: string = "Nessn dato trovato"
+  constructor(private gitHubService: ApiGithHubServise) {}
   
 
   ngOnInit() {
     this.getRepos()
   }
 
+  /*public getRepos() {
+    this.repos=[];
+    return this.http.get<GitHubRepos[]>( this.baseUrl + 'users/' + this.userName + '/repos')
+      .subscribe({
+        next: (response: GitHubRepos[]) => {
+          if (response.length !== 0) {
+            response.map((item: { id: any; name: any; html_url: any; description: any; }) => {
+              this.repos.push({
+                id: item.id,
+                name: item.name,
+                html_url: item.html_url,
+                description: item.description
+              });
+            });
+            this.loaded = true;
+          }
+        },
+        error: (err): void => {
+          this.errorMessage = {
+            statusCode: err.status,
+            message: err.error.message,
+          };
+          this.loaded = false;
+        },
+        complete: () => {
+          console.log('Request completed.');
+          this.loaded = true;
+        },
+      });
+  }*/
+
   public getRepos(){
-    return this.http.get<GitHubRepos[]>(this.baseUrl + 'users/' + this.userName + '/repos')
+    this.repos=[];
+    this.gitHubService.getRepos(this.userName)
     .subscribe({
-      next: (response) => {     //Next callback
-        this.repos=response;
-      },   
-      (error) => {        //Error callback
-        this.repos=[];
-        this.errorMessage=error;
-      },      
-      () => {             //Complete callback
-        console.log('GitHubRepos Completed');
+      next: (response: GitHubRepos[]) => {
+        if (response.length != 0) {
+          response.map((item: { id: any; name: any; html_url: any; description: any; }) => {
+            this.repos.push({
+              id: item.id,
+              name: item.name,
+              html_url: item.html_url,
+              description: item.description
+            });
+          });
+          this.loaded = true;
+          this.no_data_found = false;
+        }
+        else{
+          this.loaded = false;
+          this.no_data_found = true;
+        }
+          
+      },
+      error: (err): void => {
+        this.errorMessage = {
+          statusCode: err.status,
+          message: err.error.message,
+        };
+        this.loaded = false;
+      },
+      complete: () => {
+        console.log('Request completed.');
+        //this.loaded = true;
       },
     });
   }
 
   cerca(contactForm:FormGroup) {
     this.getRepos()
-  }
-  cancella(contactForm:FormGroup) {
   }
 }
